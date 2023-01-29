153680d54b0682964d45453c5e69ff4062ab3da1
Hyuk <testerhyuk214@gmail.com>
2023-01-29 20:58:54 +0900

    update index

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 import useSWR from 'swr';
 import { Post, Sub } from '../types'
 import axios from 'axios'
